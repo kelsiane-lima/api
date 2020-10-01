b05bdc652bc57e84693e07625812f0444a488467
kelsiane-lima <kelsiane.lima.silva06@aluno.ifce.edu.br>
2020-10-01 18:23:23 -0300

    tirando a dependencia cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const routes = require('./routes');
-
 require("dotenv-safe").config(); 
 
 const app = express();
 const PORT =3333;
 
-app.use(cors());
 app.use(express.json());
 app.use(routes);
 
